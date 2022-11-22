f069ffe1d2badc9460b244df12e05f8cb99e5c7c
CErAasifAli <aaasifali124@gmail.com>
2022-11-21 22:18:29 -0800

    inital commit

---
+++ src/main/java/com/example/demo/Proj02SpringSchedulngTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj02SpringSchedulngTest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj02SpringSchedulngTest01Application.class, args);
+	}
+
+}
